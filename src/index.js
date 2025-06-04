7a88b927569d64d6b896bcedef9c12486ec3429d
MAC <mac@MacBook-Pro-MAC.local>
2025-06-04 18:47:06 +0200

    remove unused web-vitals and cleanup index

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,5 +14,3 @@ root.render(
         </Router>
     </Provider>
 );
-
-reportWebVitals();
